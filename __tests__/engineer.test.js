30845377be21247edecfd971bb178e83ae0e1091
Shanon Holland <105896063+sm0526@users.noreply.github.com>
2022-11-09 23:17:52 -0700

    wrote test in engineer.test.js to use a function to retrieve git hub username

--- __tests__/engineer.test.js
+++ __tests__/engineer.test.js
@@ -5,3 +5,9 @@ test("Using constructor arguments can set GitHub username", () => {
     const x = new Engineer("Snow", 1, "test@email.com", testGitHub);
     expect(x.github).toBe(testGitHub);
 });
+
+test("Using a function can retieve GitHub username", () => {
+    const testGitHub = "GitHubUserName";
+    const x = new Engineer("Snow", 1, "test@email.com", testGitHub);
+    expect(x.retrieveGitHub()).toBe(testGitHub);
+});
