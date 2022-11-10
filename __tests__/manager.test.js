848d2b940912d961667e6c4b32c5fda3f7b67ce3
Shanon Holland <105896063+sm0526@users.noreply.github.com>
2022-11-09 23:31:29 -0700

    wrote the test in manager.test.js to test a function being able to retrieve the office number

--- __tests__/manager.test.js
+++ __tests__/manager.test.js
@@ -5,3 +5,9 @@ test("Using constructor argument can set office number", () => {
     const x = new Manager("Snow", 1, "test@email.com", testOffice);
     expect(x.officeNumber).toBe(testOffice);
 });
+
+test("Using a function can retrieve the office number", () => {
+    const testOffice = 100;
+    const x = new Manager("Snow", 1, "test@email.com", testOffice);
+    expect(x.retrieveOfficeNumber()).toBe(testOffice);
+});
