7d614d501c083f6e9b53a6ff5741feb0dc174359
Shanon Holland <105896063+sm0526@users.noreply.github.com>
2022-11-09 23:25:52 -0700

    wrote the test for a function to retrieve the school name in intern.test.js

--- __tests__/intern.test.js
+++ __tests__/intern.test.js
@@ -5,3 +5,9 @@ test("Using constructor arguments can set school", () => {
     const x = new Intern("Snow", 1, "test@email.com", testSchool);
     expect(x.school).toBe(testSchool);
 });
+
+test("Using a function to retrieve the school name", () => {
+    const testSchool = "DU";
+    const x = new Intern("Snow", 1, "test@email.com", testSchool);
+    expect(x.retrieveSchool()).toBe(testSchool);
+});
