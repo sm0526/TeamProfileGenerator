713c65a7b3ae7aa1e3bd42e6acbbf3f0f3df5082
Shanon Holland <105896063+sm0526@users.noreply.github.com>
2022-11-10 10:50:30 -0700

    wrote the code in index.js, the inquirer code to prompt and varify the responses for the team manager

--- index.js
+++ index.js
@@ -11,3 +11,67 @@ const DIST_DIR = path.resolve(__dirname, 'dist');
 const distPath = path.join(DIST_DIR, 'team.html');
 const idArray = [];
 const teamMembers = [];
+//instructions to reset
+console.log(
+    '\nUse `npm run reset` to reset the dist folder\n'
+);
+//functions
+function appMenu() {
+    function constructManager() {
+        console.log('Build your Team');
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    name: 'managerName',
+                    message: 'Who is the team manager?',
+                    validate: (answer) => {
+                        if (answer !== '') {
+                            return true;
+                        } else {
+                            return 'Enter at least one character';
+                        };
+                    },
+                },
+                {
+                    type: 'input',
+                    name: 'managerId',
+                    message: 'What is the id of the team manager?',
+                    validate: (answer) => {
+                        const qualify = answer.match(/^[1-9]\d*$/);
+                        if (qualify) {
+                            return true;
+                        } else {
+                            return 'Enter a number greater than zero';
+                        };
+                    },
+                },
+                {
+                    type: 'input',
+                    name: 'managerEmail',
+                    message: "What is the manager's email address?",
+                    validate: (answer) => {
+                        const qualify = answer.match(/\S+@\S+\.\S+/);
+                        if (qualify) {
+                            return true;
+                        } else {
+                           return 'Enter a valid email address';
+                        };
+                    },
+                },
+                {
+                    type: 'input',
+                    name: 'managerOfficeNumber',
+                    message: "What is the manager's office number?",
+                    validate: (answer) => {
+                        const qualify = answer.match(/^[1-9]\d*$/);
+                        if (qualify) {
+                            return true;
+                        } else {
+                            return 'Enter a number greater than zero';
+                        };
+                    },
+                },
+            ])
+    }
+}
