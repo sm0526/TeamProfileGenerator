5bfbe3afc391f701562d3fe35058cda486d2679f
Shanon Holland <105896063+sm0526@users.noreply.github.com>
2022-11-09 23:08:50 -0700

    wrote tests for 3 functions to retreive employee info for employee.test.js

--- __tests__/employee.test.js
+++ __tests__/employee.test.js
@@ -22,3 +22,21 @@ test("Using constructor arguments can set an email", () => {
     const x = new Employee("Snow", 1, testEmail);
     expect(x.email).toBe(testEmail);
 });
+
+test("Using a function can retrieve name", () => {
+    const testName = "Snow";
+    const x = new Employee(testName);
+    expect(x.retrieveName()).toBe(testName);
+});
+
+test("Using a funciton can retrieve id", () => {
+    const testId = 100;
+    const x = new Employee("Snow", testId);
+    expect(x.retrieveId()).toBe(testId);
+});
+
+test("Using a function can retrieve email", () => {
+    const testEmail = "test@email.com";
+    const x = new Employee("Snow", 1, testEmail);
+    expect(x.retrieveEmail()).toBe(testEmail);
+})
